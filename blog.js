dd670fe2654b934fc3f308be4550eebb211406fd
Ana Paula de Souza <107646623+Anna-Souza@users.noreply.github.com>
2023-10-20 22:39:49 -0300

    Testes Blog - parte 2

--- blog.js
+++ blog.js
@@ -1,8 +1,25 @@
- const headers = document.getElementsByClassName("materia"),
-        contents = document.getElementsByClassName("content"),
-        icons = document.getElementsByClassName("abrir");
-	 
-for (let i = 0; i < headers.length; i++) {
-     headers[i].addEventListener("click", () => { contents[i].style.display = contents[i].style.display == "block" ? "none" : "block";
-     });
-}
+
+(function () {
+
+
+    const headers = document.getElementsByClassName("visible"),
+        contents = document.getElementsByClassName("hidden"),
+        icons = document.getElementsByClassName("open");
+
+    for (let i = 0; i < headers.length; i++) {
+        headers[i].addEventListener("click", () => {
+
+            for (let j = 0; j < contents.length; j++) {
+                if (i == j) {
+                    icons[j].innerHTML = contents[j].getBoundingClientRect().height === 0 ? "-" : "+";
+                    contents[j].classList.toggle("hidden-transition");
+                } else {
+                    icons[j].innerHTML = "+";
+                    contents[j].classList.remove("hidden-transition");
+                }
+            }
+
+        });
+    }
+
+})()
